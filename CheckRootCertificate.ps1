374147e4e6aa7dfe29c77f1db539f803bd8115ce
tinuwalther <it@martin-walther.ch>
2018-03-30 16:56:24 +0200

    Update CheckRootCertificate.ps1

--- CheckRootCertificate.ps1
+++ CheckRootCertificate.ps1
@@ -1,1 +1,16 @@
-Get-ChildItem Cert:\LocalMachine\Root | Where-Object NotAfter -lt (Get-Date) | Select-Object FriendlyName,DnsNameList,NotBefore,NotAfter
+function Get-ExpiredCertificates{
+    [CmdletBinding()]
+    param()
+    $function = $($MyInvocation.MyCommand.Name)
+    $ret = $null
+    try{
+        $ret = Get-ChildItem Cert:\LocalMachine\Root -ExpiringInDays 0
+    }
+    catch{
+        Write-verbose "$($function): $($_.Exception.Message)"
+        $Error.Clear()
+    }
+    return $ret
+}
+
+Get-ExpiredCertificates | Select-Object Thumbprint,FriendlyName,Issuer,NotBefore,NotAfter
