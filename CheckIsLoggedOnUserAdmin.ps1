752728689ef3f5974e781d2403b1e42123f0e18a
tinuwalther <it@martin-walther.ch>
2018-03-30 15:39:22 +0200

    Create CheckIsLoggedOnUserAdmin.ps1

---
+++ CheckIsLoggedOnUserAdmin.ps1
@@ -0,0 +1,27 @@
+function Test-IsUserMemberOfAdmin{
+    [CmdletBinding()]
+    param(
+        [String]$LocalAccount
+    )
+    $function = $($MyInvocation.MyCommand.Name)
+    $ret = $false
+    try{
+        $Administrators = Get-LocalGroupMember -SID S-1-5-32-544  
+        if($Administrators.Name -match "$LocalAccount"){   
+            $ret = $true   
+        }     
+    }
+    catch{
+        Write-verbose "$($function): $($_.Exception.Message)"
+        $Error.Clear()
+        $ret = $false
+    }
+    return $ret
+}
+
+$LoggedOnUsers = (Get-CimInstance Win32_LoggedOnUser).antecedent.name | Select-Object -Unique
+foreach($item in $LoggedOnUsers){ 
+    if(Test-IsUserMemberOfAdmin -LocalAccount "$($env:ComputerName)\\$item"){
+        @{$item=LocalAdmin}
+    }
+}
