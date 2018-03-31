0be2411cdd5dc474035a3b61ec540aba983b767e
tinuwalther <it@martin-walther.ch>
2018-03-31 19:58:39 +0200

    Update GenerateHealthReport.ps1

--- GenerateHealthReport.ps1
+++ GenerateHealthReport.ps1
@@ -4,7 +4,7 @@ $HTMLHeader = @"
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN" "http://www.w3.org/TR/html4/frameset.dtd">
 <html>
 <head>
-<title>Systems Report</title>
+<title>System Report</title>
 <h1 id="home">Health report of computer $($env:COMPUTERNAME)</h1>
 <style>
 
@@ -100,6 +100,9 @@ footer {
 <body>
 <ul>
   <li><a class="active" href="#home">Home</a></li>
+  <li><a href="#uptime">Uptime</a></li>
+  <li><a href="#memory">Memory</a></li>
+  <li><a href="#disks">Disks</a></li>
   <li><a href="#logs">Eventlogs</a></li>
   <li><a href="#services">Services</a></li>
   <li><a href="#about">About</a></li>
@@ -117,6 +120,81 @@ $HTMLEnd = @"
 </div>
 </html>
 "@
+
+function Get-HostUptime {
+    [CmdletBinding()]
+    param(
+    )
+    $function = $($MyInvocation.MyCommand.Name)
+    $ret = @()
+    try{
+	    $Uptime = Get-WmiObject -Class Win32_OperatingSystem
+	    $LastBootUpTime = $Uptime.ConvertToDateTime($Uptime.LastBootUpTime)
+	    $Time = (Get-Date) - $LastBootUpTime
+        $obj = [PSCustomObject]@{
+            Days    = "{0:00}" -f $Time.Days
+            Hours   = "{0:00}" -f $Time.Hours
+            Minutes = "{0:00}" -f $Time.Minutes
+            Seconds = "{0:00}" -f $Time.Seconds
+        }
+        $ret  += $obj
+    }
+    catch{
+        Write-verbose "$($function): $($_.Exception.Message)"
+        $Error.Clear()
+    }
+    return $ret
+}
+
+function Get-Raminfo{
+    [CmdletBinding()]
+    param()
+    $function = $($MyInvocation.MyCommand.Name)
+    $ret = @()
+    try{
+	    Get-WmiObject -Class Win32_OperatingSystem -ErrorAction Stop | Select-Object TotalVisibleMemorySize,FreePhysicalMemory | %{
+            $TotalRamMB    = [math]::round(($_.TotalVisibleMemorySize/(1024*1024)),2)
+            $FreeRamMB     = [math]::round(($_.FreePhysicalMemory/(1024*1024)),2)
+            $obj = [PSCustomObject]@{
+                TotalRamGB     = $TotalRamMB
+                FreeRamGB      = $FreeRamMB
+                UsedRamGB      = $TotalRamMB - $FreeRamMB
+                RamPercentFree = [math]::round((($FreeRamMB / $TotalRamMB) * 100),2)
+            }
+            $ret  += $obj
+        }
+    }
+    catch{
+        Write-verbose "$($function): $($_.Exception.Message)"
+        $Error.Clear()
+    }
+    return $ret
+}
+
+function Get-Diskinfo{
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$true)][Int]$thresholdspace
+    )
+    $function = $($MyInvocation.MyCommand.Name)
+    $ret = @()
+    try{
+	    Get-WMIObject Win32_LogicalDisk -ErrorAction Stop| Where-Object{$_.DriveType -eq 3} | Where-Object{ ($_.freespace/$_.Size)*100 -lt $thresholdspace} | %{
+            $obj = [PSCustomObject]@{
+                Name        = $_.Name
+                TotalSizeGB = [math]::round(($_.size/1gb),2)
+                FreeSpaceGB = [math]::round(($_.freespace/1gb),2)
+                PercentFree = [math]::round(($_.freespace/$_.size*100),2)
+            }
+            $ret  += $obj
+        }
+    }
+    catch{
+        Write-verbose "$($function): $($_.Exception.Message)"
+        $Error.Clear()
+    }
+    return $ret
+}
 
 function Get-LastEventCodes{
     [CmdletBinding()]
@@ -157,7 +235,7 @@ function Get-StoppedServices{
     $function = $($MyInvocation.MyCommand.Name)
     $ret = @()
     try{
-        Get-WmiObject Win32_Service | Where-Object StartMode -eq 'Auto' | Where-Object State -eq 'Stopped' | %{
+        Get-WmiObject Win32_Service -ErrorAction Stop | Where-Object StartMode -eq 'Auto' | Where-Object State -eq 'Stopped' | %{
             $obj = [PSCustomObject]@{
                 Name        = $_.Name
                 DisplayName = $_.DisplayName
@@ -203,36 +281,41 @@ $ret += @"
 
 }
 
-function Set-HtmlMiddleServices{
-    [CmdletBinding()]
-    param(
-        [Parameter(Mandatory=$true)][Object]$htmlTable
-    )
-    $function = $($MyInvocation.MyCommand.Name)
-    $ret = $null
-    try{
-$ret += @"
-<div>
-<p>The following is a list of all <b>stopped services</b> with start-mode <b>automatic</b>.</p>
-<table>$htmlTable</table>
-</div>
-"@
-    }
-    catch{
-        Write-verbose "$($function): $($_.Exception.Message)"
-        $Error.Clear()
-    }
-    return $ret
-
-}
-
 $lastdays    = 2
 $HTMLMiddle  = $null
 
-#region Services
-$HTMLMiddle  += '<h2 id="services">Services</h2>'
-$htmlTable   = Get-StoppedServices | ConvertTo-Html -Fragment 
-$HTMLMiddle  += Set-HtmlMiddleServices -htmlTable $htmlTable 
+#region HostUptime
+$HTMLMiddle  += '<h2 id="uptime">Uptime</h2>'
+$htmlTable   = Get-HostUptime | ConvertTo-Html -Fragment 
+$HTMLMiddle  += @"
+<div>
+<p>The following is the <b>last boot-time</b> for the computer.</p>
+<table>$htmlTable</table>
+</div>
+"@
+#endregion
+
+#region Memory
+$HTMLMiddle  += '<h2 id="memory">Memory</h2>'
+$htmlTable   = Get-Raminfo -Verbose | ConvertTo-Html -Fragment 
+$HTMLMiddle  += @"
+<div>
+<p>The following list the memory usage.</p>
+<table>$htmlTable</table>
+</div>
+"@
+#endregion
+
+#region Diskinfo
+$PercentFree  = 50
+$HTMLMiddle  += '<h2 id="disks">Disks</h2>'
+$htmlTable   = Get-Diskinfo -thresholdspace $PercentFree -Verbose | ConvertTo-Html -Fragment 
+$HTMLMiddle  += @"
+<div>
+<p>The following list the disks with <b>less than $PercentFree%</b> free space.</p>
+<table>$htmlTable</table>
+</div>
+"@
 #endregion
 
 #region Eventlogs
@@ -246,10 +329,22 @@ $LogsToCheck = @(
 foreach($item in $LogsToCheck){
     $records = Get-LastEventCodes -Logname $item -day $lastdays -Verbose
     if(-not([String]::IsNullOrEmpty($records))){
+        $records | Sort-Object TimeGenerated -Descending | ConvertTo-Json | ConvertFrom-Json
         $htmlTable = $records | Sort-Object TimeGenerated -Descending | ConvertTo-Html -Fragment 
         $HTMLMiddle += Set-HtmlMiddleEventlog -Logname $item -lastdays $lastdays -htmlTable $htmlTable 
     }
 }
+#endregion
+
+#region Services
+$HTMLMiddle  += '<h2 id="services">Services</h2>'
+$htmlTable   = Get-StoppedServices -Verbose | ConvertTo-Html -Fragment 
+$HTMLMiddle  += @"
+<div>
+<p>The following is a list of all <b>stopped services</b> with start-mode <b>automatic</b>.</p>
+<table>$htmlTable</table>
+</div>
+"@
 #endregion
 
 $HTMLmessage = $HTMLHeader + $HTMLMiddle + $HTMLEnd
