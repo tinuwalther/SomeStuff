1baa519b54943b2cb351cb5b343521ee264c27d5
Martin Walther <it@martin-walther.ch>
2019-08-02 11:52:35 +0200

    Create HeadlessChrome.ps1

---
+++ HeadlessChrome.ps1
@@ -0,0 +1,6 @@
+
+$path = "D:\Output\Report_20190731-123445"
+$url  = "file:///$($path -replace '\\', '/')"
+
+$chrome = "$(${env:ProgramFiles(x86)})\Google\Chrome\Application\chrome.exe"
+& $chrome --headless --disable-gpu --print-to-pdf="$($path).pdf" "$($url).html"
